4f22c4ac7aa4af539e4a71fa53548d223216e8dc
Sam Thursfield <sam@afuera.me.uk>
2021-09-06 14:07:34 +0100

    Add more to VST

--- vst/src/lib.rs
+++ vst/src/lib.rs
@@ -1,10 +1,19 @@
+// VST2 plugin for Boucle.
+//
+// Following: https://vaporsoft.net/creating-an-audio-plugin-with-rust-vst/
+
 #[macro_use]
 extern crate vst;
 
-use vst::plugin::{Info, Plugin, Category};
+use vst::api::Events;
+use vst::buffer::AudioBuffer;
+use vst::event::Event;
+use vst::plugin::{Category, Info, Plugin};
 
 #[derive(Default)]
 struct BoucleVst;
+
+type VstSample = f32;
 
 impl Plugin for BoucleVst {
     fn get_info(&self) -> Info {
@@ -17,6 +26,27 @@ impl Plugin for BoucleVst {
             version: 1,
             category: Category::Effect,
             ..Default::default()
+        }
+    }
+
+    fn process_events(&mut self, events: &Events) {
+        for event in events.events() {
+            match event {
+                Event::Midi(ev) => {
+                    println!("Got MIDI event: {}.", ev.data[0]);
+                },
+                _ => (),
+            }
+        }
+    }
+
+    fn process(&mut self, buffer: &mut AudioBuffer<VstSample>) {
+        let (input_buffer, mut output_buffer) = buffer.split();
+
+        for output_channel in output_buffer.into_iter() {
+            for output_sample in output_channel {
+                *output_sample = 0f32;
+            }
         }
     }
 }
