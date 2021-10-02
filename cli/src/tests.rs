73a55e1084c80def66b3298a038ff84fc6b7e0ad
Sam Thursfield <sam.thursfield@codethink.co.uk>
2021-10-02 13:21:08 +0100

    cli/tests: Put test output to /tmp/boucle-tests
    We don't clean this up yet, but at least it's all in one place.

--- cli/src/tests.rs
+++ cli/src/tests.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
     use std::path::PathBuf;
 
     use crate::run_batch;
@@ -15,17 +16,34 @@ mod tests {
             .to_string();
     }
 
+    fn get_test_output_path(filename: &str) -> String {
+        let mut path = std::env::temp_dir();
+        path.push("boucle-tests");
+        std::fs::create_dir_all(path.to_str().unwrap());
+        path.push(filename);
+        return path
+            .to_str().unwrap()
+            .to_string();
+    }
+
     #[test]
     fn test_batch_i16() {
         let ops_path = get_test_data_path("ops.test");
         let input_path = get_test_data_path("chirp.i16.wav");
-        run_batch(&input_path, "/tmp/foo.wav", &ops_path);
+        let output_path = get_test_output_path("out.i16.wav");
+        run_batch(&input_path, &output_path, &ops_path);
+
+        assert!(Path::new(&output_path).exists(),
+            "Output {} does not exist", output_path);
     }
 
     #[test]
     fn test_batch_f32() {
         let ops_path = get_test_data_path("ops.test");
         let input_path = get_test_data_path("chirp.f32.wav");
-        run_batch(&input_path, "/tmp/foo.wav", &ops_path);
+        let output_path = get_test_output_path("out.f32.wav");
+        run_batch(&input_path, &output_path, &ops_path);
+
+        assert!(Path::new(&output_path).exists());
     }
 }
