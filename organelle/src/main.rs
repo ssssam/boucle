5eba26c8df943b77c444760470a2b1719ebbbe3a
Sam Thursfield <sam@afuera.me.uk>
2021-10-15 22:27:37 +0200

    Initial Organelle support
    Still to do:
    
      * Read input on /keys messages and handle them appropriately
      * Write something to organelle screen
      * Bundle with a run.sh script as a real organelle patch

---
+++ organelle/src/main.rs
@@ -0,0 +1,28 @@
+#[cfg(feature = "organelle")]
+pub mod boucle_organelle {
+    use nannou_osc as osc;
+
+    pub fn main() {
+        let sender: osc::Sender::<osc::Connected>;
+
+        let port = 4001;
+        let target_addr = format!("{}:{}", "127.0.0.1", port);
+
+        let sender = osc::sender()
+            .expect("Could not bind to default socket")
+            .connect(target_addr)
+            .expect("Could not connect to socket at address");
+
+        let osc_addr = "/oled/line/1".to_string();
+        let args = vec![osc::Type::String("Hello from Boucle looper".to_string())];
+        let packet = (osc_addr, args);
+        sender.send(packet).ok();
+
+        println!("Sent a hello packet.")
+    }
+}
+
+#[cfg(feature = "organelle")]
+fn main() {
+    boucle_organelle::main();
+}
